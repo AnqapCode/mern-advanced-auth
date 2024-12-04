558ad4720a9573b34732c5bff2e71a52c9ffad4c
AnqapCode <arifcahyogd7@gmail.com>
2024-12-04 10:48:13 +0700

    Port ke env

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ import authRoutes from "./routes/auth.routes.js";
 dotenv.config();
 
 const app = express();
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // production
 // const __dirname = path.resolve();
@@ -38,7 +38,7 @@ app.use("/api/auth", authRoutes);
 //   });
 // }
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log(`Server is running on port 5000`);
+  console.log(`Server is running on port ${PORT}`);
 });
